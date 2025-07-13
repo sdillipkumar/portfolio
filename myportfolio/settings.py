c9bdd3c159027021e33fb1688d17019e1b6b14b1
Dilip kumar singh <117737382+sdillipkumar@users.noreply.github.com>
2025-07-13 20:29:43 +0530

    Update settings.py

--- myportfolio/settings.py
+++ myportfolio/settings.py
@@ -76,15 +76,10 @@ WSGI_APPLICATION = 'myportfolio.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.postgresql',
-        'NAME': 'portfolio',
-        'USER': 'postgres',     
-        'PASSWORD': 'Dillip@00936163',  
-        'HOST': 'localhost',
-        'PORT': '5432',
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / "db.sqlite3",
     }
 }
-
 
 # Password validation
 # https://docs.djangoproject.com/en/5.2/ref/settings/#auth-password-validators
